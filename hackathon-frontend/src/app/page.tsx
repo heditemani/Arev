73a53fd6b493e398b91dacaac934fe891ac9151e
Hedi Temani <hedi.temani97@gmail.com>
2026-05-14 17:20:28 +0100

    refactor: update .gitignore, remove pnpm-workspace.yaml, and enhance page layout with login form

--- hackathon-frontend/src/app/page.tsx
+++ hackathon-frontend/src/app/page.tsx
@@ -1,7 +1,37 @@
-"use client";
+'use client'
+import Link from "next/link"
+import styles from "./page.module.css"
 
-const HomePage = () => {
-  return <div>Hello</div>;
-};
+const page = () => {
+  return (
+    <div className={styles.page_container}>
+      <h1>Arev</h1>
+      <div className={styles.login_form}>
+        <form className="" action="">
+          <h2 className={styles.login_txt}>Login!</h2>
+          <p>Please enter your credentials below to continue</p>
+          <label htmlFor="">Username</label>
+          <input type="text" placeholder="Enter your username" />
 
-export default HomePage;
+          <label htmlFor="">Password</label>
+          <input type="password" placeholder="Enter your password" />
+          <div className={styles.remeber_me_section}>
+            <div className={styles.remeber_me}>
+              <input type="checkbox" />
+              <span>Remeber me</span>
+            </div>
+            <Link href="">
+              <span className={styles.forget_password}>Forget Password?</span>
+            </Link>
+
+          </div>
+
+          <input className={styles.login_btn} type="submit" value="Login" />
+
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default page
